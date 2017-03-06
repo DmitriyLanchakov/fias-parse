7d0b044bd50663da0522bde2ada49a6748e75fad
Afanasy Argunov <afanasy@argunov.com>
2017-03-06 16:55:20 +0900

    JSON parser added

--- index.js
+++ index.js
@@ -1,44 +1,76 @@
-var Parser = require('node-dbf');
+var xml2js = require('xml2js');
 var fs = require('fs');
 var path = require('path');
 var ps = require('process');
-var _ = require('underscore');
 var async = require('async');
+
+var cluster = require('cluster');
+
+var fias_path = process.env.FIAS_PATH || __dirname + '/fias';
+var out_path = process.env.OUT_PATH;
+
 async.waterfall([
   function (cb) {
     require('./lib/mongoose').connect(cb);
   },
   function (r, cb) {
     console.log(r);
-    var fias_path = process.env.FIAS_PATH || __dirname + '/fias';
-    var out_path = process.env.OUT_PATH;
     console.log('Попытка прочитать файлы ФИАС', fias_path);
-    fs.readdir(fias_path, function (e, f) {
-      if (e) { cb(e); }
-      else {
-        _.each(f, function (i) {
-          if (out_path && path.extname(i) === '.DBF') {
-            var file = path.join(out_path, i);
-            var file_to_parse = path.join(fias_path, i);
-            var parser = new Parser(file_to_parse);
-            parser.on('end', function (p) {
-              process.stdout.write('Файл ' + i + ' завершен\r');
-            });
-            parser.on('record', function (record) {
-              if (!fs.existsSync(file)) {
-                fs.writeFileSync(file, record);
-              } else {
-                fs.appendFileSync(file, record);
+    fs.readdir(fias_path, cb);
+  },
+  function (f, cb) {
+    async.eachSeries(f, function (i, cbb) {
+      if (out_path && path.extname(i) === '.XML') {
+        var file = path.join(out_path, i);
+        var file_to_parse = path.join(fias_path, i);
+        var parser = new xml2js.Parser();
+        var rs = fs.createReadStream(file_to_parse);
+        var data = null;
+        var count = 0;
+
+        rs.on('data', function (chunk) {
+          data += chunk;
+          count++;
+          if (cluster.isMaster) {
+            for (var i = 0; i < 2; ++i) {
+              cluster.fork();
+            }
+          } else {
+            parser.parseString(data, function (e, result) {
+              if (e) { return; }
+              console.log('Обработано записей ', count);
+              if (result) {
+                var ws;
+                fs.stat(file_to_parse, function (e, stats) {
+                  if (e) {
+                    if (e.code === 'ENOENT') {
+                      ws = fs.createWriteStream(file + '.json', { flags: 'w' });
+                      ws.write(JSON.stringify(result));
+                      ws.end();
+                    }
+                  }
+                  ws = fs.createWriteStream(file + '.json', { flags: 'a' });
+                  ws.write(JSON.stringify(result));
+                  ws.end();
+                });
               }
             });
-            parser.parse();
-          } else {
-            // MongoDB write here
           }
         });
+
+        rs.on('end', function () {
+          data = null;
+          count = 0;
+          console.log('Файл ' + file_to_parse + ' обработан');
+        });
+      } else {
+        // MongoDB write here
       }
+      cbb();
+    }, function (e) {
+      if (e) { cb(e); }
     });
-    cb(null, '')
+    cb(null);
   }
 ], function (e, m) {
   if (e) { console.log(e); }
